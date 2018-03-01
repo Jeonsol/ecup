8a507b01f1c57a2f4ae8fda62155f9a951003a7d
전솔 <jeonsol@nhn.com>
2018-03-01 23:01:15 +0900

    자주 쓰는 기능 구현

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 전솔 테스트 환경
+
+1. 특정 부모요소, 특정 자식 요소 가져오는 것 수정해야 됨(jquery 참고) 
+    - 예) $(this).parents(this.target)
---
+++ js/ecup.js
@@ -0,0 +1,53 @@
+var ecup = function(selector) {
+  function Ecup()  {
+   this.target = document.querySelectorAll(selector);
+  }
+  Ecup.prototype = {
+    colorChange: function(event,color) {
+        let objThis;
+        let fn = function() {
+            this.style.color = color;
+            if(event === 'mouseover') {
+                objThis = this;
+                objThis.onmouseout = function() {
+                    objThis.style.color = "";
+                }
+            }
+        };
+        this.target.forEach(data => {
+            data.addEventListener(event,fn,false);
+        });
+        return this;
+    },
+    aria: function(event,aria,boolData) {
+        let fn = function() {
+            let siblings = this.parentNode.children;
+            Array.from(siblings).forEach(function (data) {
+                data.setAttribute(aria,!boolData);
+            });
+            this.setAttribute(aria,boolData);
+        };
+        this.target.forEach(data => {
+            data.addEventListener(event,fn,false);
+        });
+        return this;
+    },
+    layerToggle : function(target,open, close) {
+        let objThis = this;
+        let fn_layer_open = function() {
+            objThis.target[0].querySelector(target).setAttribute('style','display:block;');
+        };
+        let fn_layer_close = function() {
+            objThis.target[0].querySelector(target).setAttribute('style','display:none;');
+        };
+        this.target.forEach(parent => {
+           let btn_open = parent.querySelector(open);
+           let btn_close = parent.querySelector(close);
+           btn_open.addEventListener('click', fn_layer_open, false);
+           btn_close.addEventListener('click', fn_layer_close, false);
+        });
+        return this;
+    }
+  };
+  return new Ecup();
+};
