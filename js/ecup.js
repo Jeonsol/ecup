f204ccabdc933e3ed539bafe3c6e879b565e0df2
NT60328 <sunah-lee@nhn.com>
2018-03-02 16:38:02 +0900

    .makeBtn 함수 구현

--- js/ecup.js
+++ js/ecup.js
@@ -1,4 +1,7 @@
 if(typeof(Module) == "undefined") var Module = {};
+if(typeof(winModule) == "undefined") var winModule = {};
+var newWindow;
+
 (function(doc){
     Module.Element = function(){
         this.initalize.apply(this,arguments);
@@ -7,6 +10,8 @@ if(typeof(Module) == "undefined") var Module = {};
     Module.Element.prototype = {
         initalize : function(target) {
             this.dom =  doc.querySelector(target);
+            this.target = target;
+            this.windows =  newWindow;
         },
 
         init_arguemnt : function(){
@@ -15,31 +20,39 @@ if(typeof(Module) == "undefined") var Module = {};
 
         style : function(css){
             var dom = this.dom;
+            this.style = css;
             if(this.eventType != null) {
                 dom.addEventListener(this.eventType, function(){
-                    dom.setAttribute('style', css);
+                    var originData = dom.getAttribute('style');
+                    if(originData != null)
+                        dom.setAttribute('style', originData + css);
+                    else
+                        dom.setAttribute('style', css);
                 })
             }else {
-                dom.setAttribute('style', css);
+                var originData = dom.getAttribute('style');
+                if(originData != null)
+                    dom.setAttribute('style', originData + css);
+                else
+                    dom.setAttribute('style', css);
             }
 
             this.init_arguemnt();
             return this;
         },
 
-        event : function(type){
+        event : function(eventType){
             var dom = this.dom;
-            this.eventType = type;
+            this.eventType = eventType;
             return this;
         },
 
         addClass : function(className){
             var dom = this.dom;
-
+            this.class = className;
             if(this.eventType != null) {
                 dom.addEventListener(this.eventType, function(){
                     dom.className += ' ' + className;
-
                 })
             }
             else {
@@ -47,9 +60,84 @@ if(typeof(Module) == "undefined") var Module = {};
             }
             return this;
         },
+
+        makeBtn : function(name){
+            this.windows.init(this, name);
+            return this;
+        }
     }
+
+    winModule.Page = function(){
+        this.initalize.apply(this,arguments);
+    }
+
+    winModule.Page.prototype = {
+        initalize : function(name) {
+            this.obj = [];
+            this.window = window.open('', 'newWindow', 'width=300, height=500');
+        },
+
+        init : function(obj, name){
+            obj.name = name;
+            console.log(obj);
+            this.obj.push(obj);
+            for(var index in obj) {
+                if (obj.hasOwnProperty(index)) {
+                    var attr = obj[index];
+                    var originData = doc.querySelector(obj.target).getAttribute(index);
+
+                    if(originData != null){
+                        if(index == 'style')
+                            doc.querySelector(obj.target).setAttribute(index, originData.replace(obj.style, '')  );
+                        else
+                            doc.querySelector(obj.target).setAttribute(index, originData.replace(attr, '')  );
+                    }
+               }
+           }
+           this.window.document.body.innerHTML = this.html();
+        },
+
+        html : function(){
+            var context = this;
+            var html = '';
+
+            this.obj.forEach(function(value){
+                html = html + '<button onclick="' + context.script(value) +'">'+value.name+'</button>';
+            });
+
+            return html;
+        },
+
+        // 명세서 script ( in button )
+        script : function(obj){
+            console.log(obj);
+            var script = '';
+            script = script + makeScript();
+            return script;
+
+            function makeScript(){
+                var code = "window.opener.document.querySelector('" + obj.target + "')";
+
+                for(var index in obj) {
+                    if (obj.hasOwnProperty(index)) {
+                        var attr = obj[index];
+                        if(index == 'style' || index == 'class')
+                            code = code + ".setAttribute('" + index + "', '"+ attr +" ')"
+                   }
+                }
+                return code;
+            }
+        },
+
+        _ : function(target){
+            return new Module.Element(target);
+        }
+    }
+
 })(document);
 
 function _(target){
     return new Module.Element(target);
 }
+
+newWindow =  new winModule.Page();
