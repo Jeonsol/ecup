f223d826adbf9ca0aa5b21a5d6b433c0ad22d16e
김재원 <kjwvnek@nhn.com>
2018-03-08 15:40:55 +0900

    메인함수 정리

--- js/ecup.js
+++ js/ecup.js
@@ -28,36 +28,118 @@
 2. type에 따른 DOM(버튼.. 등) 생성과 이벤트 붙이기 (팝업창은 document.write, 기타는 나머지 방법?아마도 dom 생성에서 body에 추가)
 
  */
+/*
+*
+* 메인함수 기본 구조
+*
+* 사용자 기준으로 single, group > 그리는 메소드
+* draw > 내부적으로 그리는 함수
+*
+* */
 function markupLayer() {
 
-    // type check => 뷰처리 함수 실행
-    var obj = codeParsing(code, fn);
+    /* 테스트에서 ecup으로 쓰여있는함수 */
+    var Main = function(flag) {
+
+        var userObj = new Main();
+
+        if (typeof flag === 'string') {
+            // flag가 문자열인 경우, 그룹이름이기 떄문에 설정해주고 추후 group 메소드로 그린다.
+            userObj.groupName = flag;
+        } else if (typeof flag === 'object') {
+            // flag가 객체인 경우, flag가 바로 옵션이 되며 single로 그린다.
+            userObj.single(flag);
+        }
+
+        return userObj;
+    };
+
+    Main.set = function(options) {
+        if (typeof options === 'object')
+            for (var opt in options)
+                markupLayer[opt] = options[opt];
+    };
+
+    Main.prototype = {
+        constructor: Main,
+        single: function(options) {
+            /* single은 type에따라서 그리면된다. */
+            // markupLayer.type 으로 불러온다.
+            draw(options, callback /* 타입에따른 */);
+        },
+        group: function() {
+            // 마지막 인자가 옵션
+            var options = arguments[arguments.length - 1];
+            // 받아온 공통 dom의 selector들을 배열로 넣어서
+            var commonDoms = [];
+            for (var i = 0; i < arguments.length - 1; ++i)
+                commonDoms.push(arguments[i]);
+            /* group은 groupInfo를 먼저 생성 */
+            var groupInfo = {
+                groupName: this.groupName,
+                groupDom: commonDoms
+            };
+            draw(options, callback, groupInfo);
+        }
+    };
+
+    return Main;
 
 
-    function codeParsing(code, callback) {
-        var obj = {
-            dom: function(e) {
 
+
+
+    function draw(options ,callback, groupInfo /* group인 경우 groupInfo 넘어온다. */) {
+
+        callback(codeParser(), groupInfo);
+
+        function codeParser() {
+            /*
+
+            options의 형태는
+            {
+                버튼이름1: 함수1,
+                버튼이름2: 함수2
             }
-        };
+            또는
+            {
+                버튼이름1: 옵션객체1,
+                버튼이름2: 옵션객체2
+            }
 
-        callback(obj);
+            이것을 spec = {
+                버튼이름1: 함수1,
+                버튼이름2: 함수2
+            }
+            로 변환
+            즉, 옵션이 객체인 경우 이벤트 함수로 변환하는 모듈
+             */
+            var spec = {};
+            return spec;
+        }
     }
 
-    function windows() {
 
+    function windows(spec, groupInfo) {
+        /* 뷰처리 - 팝업
+            spec을 가지고 dom을 생성해서 뷰를 그려줌
+            즉, 타입에 따라서 그려주는 module
+         */
     }
 
-    function internal() {
-
+    function external(spec, groupInfo) {
+        // 뷰처리 - 내장
     }
 
-    function external() {
-
+    function internal(spec, groupInfo) {
+        // 뷰처리 - 레이어
     }
 
 }
 
+
+/*
+추후 고민할 부분
 (function main() {
 
     var ecupExtended = {
@@ -81,3 +163,4 @@ function markupLayer() {
     // 부가기능 실행
     ecupExecuted.exec();
 })();
+*/
