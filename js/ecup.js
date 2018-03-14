81473aed2e55243a5412be0529a05d712f239e01
김재원 <kjwvnek@nhn.com>
2018-03-14 12:03:13 +0900

    develop 브랜치 정리

--- js/ecup.js
+++ js/ecup.js
@@ -1,131 +1,118 @@
-/*
-[구조]
+/**
+ *
+ * Ecup.js
+ * (Easy Control Markup)
+ *
+ * Version 1.0.0
+ *
+ **/
 
-1. 메인 함수
-    jQuery 확장객체를 통해 확장
-    jQuery prototype 확장객체를 통해 확장
+(function (ecupBasic, extendJQuery, autoApply) {
 
-    부가기능
-    - 디자이너에게 코멘트 달기
-    <div></div>
+    // create $.ecup
+    jQuery.ecup = ecupBasic;
 
-2. jQuery 확장
-    - 마크업 레이어
-        1). 코드 파싱 모듈
-        2). 뷰 처리 모듈
-        2-1) external 모듈
-        2-2) internal 모듈
-        2-3) layer 모듈
+    // extend jQuery
+    extendJQuery();
 
-3. jQuery prototype 확장 (jQuery 플러그인)
-    - $().toggleAttr()
-    - $().clickToggle($(dom));
-    - ie 9 이하 애니메이션 처리
+    // autoApply
+    // TODO: 아마 window.onload 후, 설정을 가져와서 설정에따라 적용시킬 기능만 적용하면 될듯.
 
-[마크업 레이어 모듈 절차]
+})({
+    /** ecup basic **/
 
-1. 사용자 코드 파싱 (사용자 코드를 마크업레이어 DOM과 이벤트함수로 바꾸는 과정)
-2. type에 따른 DOM(버튼.. 등) 생성과 이벤트 붙이기 (팝업창은 document.write, 기타는 나머지 방법?아마도 dom 생성에서 body에 추가)
+    /** 마크업 검수 레이어 기능 **/
+    markupLayer: function() {
 
- */
-/*
-*
-* 메인함수 기본 구조
-*
-* 사용자 기준으로 single, group > 그리는 메소드
-* draw > 내부적으로 그리는 함수
-*
-* */
-function markupLayer() {
+        /* 뷰타입을 쉽게 참조하기 위한 모듈 */
+        var viewModule = {
+            'windows': windows,
+            'external': external,
+            'internal': internal
+        };
 
-    var viewModule = {
-        'windows': windows,
-        'external': external,
-        'internal': internal
-    };
+        /* $.ecup.markupLayer() 를 통해 생성되는 함수 */
+        var markupLayerManager = function(flag) {
 
-    /* 테스트에서 ecup으로 쓰여있는함수 */
-    var Main = function(flag) {
+            var userObj;
 
-        var userObj;
-
-        if (typeof flag === 'string') {
-            // flag가 문자열인 경우, 그룹이름이기 떄문에 설정해주고 추후 group 메소드로 그린다.
-            userObj = new Main();
-            userObj.groupName = flag;
-            return userObj;
-        } else if (typeof flag === 'object') {
-            // flag가 객체인 경우, flag가 바로 옵션이 되며 single로 그린다.
-            userObj = new Main();
-            userObj.single(flag);
-            return userObj;
-        }
-
-    };
-
-    Main.set = function(options) {
-        if (typeof options === 'object')
-            for (var opt in options)
-                markupLayer[opt] = options[opt];
-    };
-
-    Main.prototype = {
-        constructor: Main,
-        single: function(options) {
-            /* single은 type에따라서 그리면된다. */
-            // markupLayer.type 으로 불러온다.
-            draw(options, viewModule[markupLayer.type] /* 타입에따른 */);
-        },
-        group: function() {
-            // 마지막 인자가 옵션
-            var options = arguments[arguments.length - 1];
-            // 받아온 공통 dom의 selector들을 배열로 넣어서
-            if (arguments.length > 1) {
-                var commonDoms = [];
-                for (var i = 0; i < arguments.length - 1; ++i)
-                    commonDoms.push(arguments[i]);
+            if (typeof flag === 'string') {
+                // flag가 문자열인 경우, 그룹이름이기 떄문에 설정해주고 추후 group 메소드로 그린다.
+                userObj = new markupLayerManager();
+                userObj.groupName = flag;
+                return userObj;
+            } else if (typeof flag === 'object') {
+                // flag가 객체인 경우, flag가 바로 옵션이 되며 single로 그린다.
+                userObj = new markupLayerManager();
+                userObj.single(flag);
+                return userObj;
             }
-            /* group은 groupInfo를 먼저 생성 */
-            var groupInfo = {
-                groupName: this.groupName,
-                groupDom: commonDoms
-            };
-            draw(options, viewModule[markupLayer.type], groupInfo);
+
+        };
+
+        markupLayerManager.set = function(options) {
+            if (typeof options === 'object')
+                for (var opt in options)
+                    markupLayerManager[opt] = options[opt];
+        };
+
+        markupLayerManager.prototype = {
+            constructor: markupLayerManager,
+            single: function(options) {
+                /* single은 type에따라서 그리면된다. */
+                // markupLayer.type 으로 불러온다.
+                draw(options, viewModule[markupLayerManager.type] /* 타입에따른 */);
+            },
+            group: function() {
+                // 마지막 인자가 옵션
+                var options = arguments[arguments.length - 1];
+                // 받아온 공통 dom의 selector들을 배열로 넣어서
+                if (arguments.length > 1) {
+                    var commonDoms = [];
+                    for (var i = 0; i < arguments.length - 1; ++i)
+                        commonDoms.push(arguments[i]);
+                }
+                /* group은 groupInfo를 먼저 생성 */
+                var groupInfo = {
+                    groupName: this.groupName,
+                    groupDom: commonDoms
+                };
+                draw(options, viewModule[markupLayerManager.type], groupInfo);
+            }
+        };
+
+        // 셋팅 초기화
+        markupLayerManager.type = 'windows';
+
+        return markupLayerManager;
+
+
+
+        /** 내부 함수 **/
+
+        /* 방식에 맞게 그려주는 콘트롤러 */
+        function draw(options ,callback, groupInfo /* group인 경우 groupInfo 넘어온다. */) {
+            callback(optionObj_to_eventFunc(options, groupInfo), groupInfo);
         }
-    };
 
-    // 셋팅 초기화
-    markupLayer.type = 'windows';
+        /* 옵션 객체를 이벤트 함수로 변환해서 리턴  */
+        function optionObj_to_eventFunc(options, groupInfo) {
 
-    return Main;
-
-
-
-
-
-    function draw(options ,callback, groupInfo /* group인 경우 groupInfo 넘어온다. */) {
-
-        callback(codeParser(), groupInfo);
-
-        function codeParser() {
-
-            // 공통 dom 생성
+            // 공통 dom 을 사용하는 경우 생성
             if (groupInfo)
                 var commonTarget = $(groupInfo.groupDom.join(','));
 
-
-            // option 의 객체/함수 여부 검사
+            // 기능이 객체로 주어진 경우 함수로 변환
             for (var btnName in options) {
 
                 var option = options[btnName];
-                // 함수가 아닐경우
+
                 if (typeof option === 'object') {
 
-                    // 옵션에 타겟이 없는 경우, 공통 타겟을 할당
                     if (!option.target) option.target = commonTarget;
                     else option.target = $(option.target);
 
-                    option = parsing(option);
+                    option = convert(option);
                     options[btnName] = option;
                 }
 
@@ -133,9 +120,8 @@ function markupLayer() {
 
             return options;
 
-
-            // 옵션을 함수로 변환
-            function parsing(option) {
+            /*  기능 객체를 함수로 변환 */
+            function convert(option) {
 
                 if (!option.target) throw new Error('target 이 반드시 필요합니다...');
 
@@ -155,62 +141,91 @@ function markupLayer() {
 
             }
         }
-    }
 
 
-    function windows(spec, groupInfo) {
-        newWindow = window.open('', 'newWindow', 'width=300, height=500');
-        for(var obj in spec) {
-            var name = obj,
-                func = spec[obj];
-            $(newWindow.document.body).append(new makeBtn(name, func));
+        /* 별도의 창으로 그려줌 */
+        function windows(spec, groupInfo) {
+            var newWindow = window.open('', 'newWindow', 'width=300, height=500');
+            for(var obj in spec) {
+                var name = obj,
+                    func = spec[obj];
+                $(newWindow.document.body).append(new makeBtn(name, func));
+            }
+
+            function makeBtn(name, func){
+                var html = '<button>' + name + '</button>';
+                var btn = $.parseHTML(html);
+                $(btn).on('click', func);
+                return btn;
+            }
         }
 
-        function makeBtn(name, func){
-            var html = '<button>' + name + '</button>';
-            var btn = $.parseHTML(html);
-            $(btn).on('click', func);
-            return btn;
-        }
-    }
 
-    function external(spec, groupInfo) {
-        // 뷰처리 - 내장
-    }
-
-    function internal(spec, groupInfo) {
-        // 뷰처리 - 레이어
-    }
-
-}
-
-var ecup = {};
-ecup.markupLayer = markupLayer;
-
-
-/*
-추후 고민할 부분
-(function main() {
-
-    var ecupExtended = {
-        markupLayer: function() {
+        /* 보이지 않게 내장되게 그려줌 */
+        function internal(spec, groupInfo) {
             // TODO
         }
-    };
 
-    var ecupPrototype = {
-        toggleAttr: function() {
+        /* 화면상에 보이도록 그려줌 */
+        function external(spec, groupInfo) {
             // TODO
         }
-    };
+    }
+}, function() {
+    /** jQuery 확장 **/
 
-    // jquery 확장객체
-    jQuery.ecup.extend(ecupExtended);
+    /** 속성 다루기 addAttr, removeAttr, toggleAttr **/
 
-    // jquery prototype 확장객체
-    jQuery.fn.extend(ecupPrototype..);
+    jQuery.fn.addAttr = function() {attr_HOF.call(this, arguments, attr_PF_add);};
+    jQuery.fn.removeAttrOrigin = jQuery.fn.removeAttr;
+    jQuery.fn.removeAttr = function() {attr_HOF.call(this, arguments, attr_PF_remove);};
+    jQuery.fn.toggleAttr = function() {attr_HOF.call(this, arguments, attr_PF_toggle);};
 
-    // 부가기능 실행
-    ecupExecuted.exec();
-})();
-*/
+    function attr_HOF(attrParams, callback) {
+
+        var that = this;
+
+        Array.prototype.forEach.call(attrParams, function(attrParam) {
+
+            var attr, val, regExp;
+
+            if (typeof attrParam !== 'string') throw new Error("check a type of attribute parameter");
+
+            attrParam = $.trim(attrParam);
+            attrParam += ' ';
+
+            switch (callback) {
+                case attr_PF_remove:
+                    regExp = /([^\s,]+)(?=[\s,])/g;
+                    break;
+                case attr_PF_add:
+                case attr_PF_toggle:
+                    regExp = /([^\s,]+)="([^",]+)"(?=[\s,])/g;
+                    break;
+            }
+
+            while(true) {
+
+                var temp = regExp.exec(attrParam);
+                if (temp === null) break;
+
+                if (temp[1]) attr = temp[1];
+                if (temp[2]) val = temp[2];
+
+                callback(that, attr, val);
+
+            }
+        });
+    }
+
+    function attr_PF_add($dom, attr, val) {$dom.attr(attr, val);}
+    function attr_PF_remove($dom, attr) {$dom.removeAttrOrigin(attr);}
+    function attr_PF_toggle($doms, attr, val) {
+        $.each($doms, function(i, dom) {
+            var $dom = $(dom);
+            if ($dom.attr(attr)) $dom.removeAttrOrigin(attr);
+            else $dom.attr(attr, val);
+        });
+    }
+
+});
