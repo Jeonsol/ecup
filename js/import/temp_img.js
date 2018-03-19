edf5be1c93b4111ec6d42aa53116ba6aab7f3b30
김재원 <kjwvnek@nhn.com>
2018-03-19 14:43:53 +0900

    [temp_img] 임시이미지 텍스트 기능 추가

--- js/import/temp_img.js
+++ js/import/temp_img.js
@@ -1,6 +1,6 @@
 (function tempImg() {
 
-	var type, id;
+	var type, id, text, src_type;
 	var imgs = document.getElementsByTagName('img');
 
 	for (var i = 0; i < imgs.length; ++i) {
@@ -10,16 +10,22 @@
 		/* src 가 있는 경우 지나감 (임시 이미지 아님) */
 		if (!src || src[0] !== '#') continue;
 
+		var srcArr = src.split('_');
+		src_type = srcArr[0];
+		text = srcArr[1] ? 'y' : 'n';
+
 		/* 임시이미지인 경우 */
 		// 타입 파싱
-		var typeArr = src.match(/#(p|l|n|사람|라이프스타일|자연)(\d?)/);
+		var typeArr = src_type.match(/#(p|l|n|사람|라이프스타일|자연)(\d?)_/);
 		type = typeArr && typeArr[1] ? getImgType(typeArr[1]) : getImgType(getRandomNumber());
 		id = typeArr && typeArr[2] ? typeArr[2] : getRandomNumber();
+
+		// 텍트스 여부
 
 		// 새 src 만들기
 		var width = img.getAttribute('width') || 100,
 			height = img.getAttribute('height') || width || 100,
-			newSrc = 'http://dev.ui.naver.com/tmp/?' + width + 'x' + height + '_ecup' + type + id + '_n_CFE2F3_100';
+			newSrc = 'http://dev.ui.naver.com/tmp/?' + width + 'x' + height + '_ecup' + type + id + '_' + text + '_FFFFFF_100';
 
 		img.setAttribute('src', newSrc);
 	}
