f35c35f4700fc052fbcef6cc79774b08e60cd4bd
전솔 <jeonsol@nhn.com>
2018-03-23 18:07:39 +0900

    comment 구조 변경 완료

--- js/ecup.js
+++ js/ecup.js
@@ -10,7 +10,7 @@
 
     // create $.ecup
     jQuery.Ecup = function() {
-		var modules = ['markupLayer'];
+		var modules = ['markupLayer','comment'];
 
 		for (feature in features) {
 			if ($.inArray(feature, modules) === -1) this[feature] = features[feature];
@@ -424,6 +424,156 @@
 
 		});
 	},
+	comment: function() {
+
+		function commentManager(flag) {
+			init();
+
+			if (typeof  flag === 'string') {
+				singleWrite(arguments[0],arguments[1]);
+			}
+
+			else if(typeof flag === 'object') {
+				groupWrite(flag);
+			}
+		}
+
+		commentManager.prototype = {
+			constructor: commentManager
+		};
+
+		commentManager.targetData = [];
+		commentManager.targetDom = $('<div class="__ecup_comment_section"></div>');
+		$('body').append(commentManager.targetDom);
+
+		return commentManager;
+
+		function init() {
+			var $elementTarget = [];
+			for(var i = 0; i < document.all.length; i++) {
+				var $element = $(document.all[i]);
+				if($element.css('overflow') === 'scroll' || $element.css('overflow') === 'auto' || $element.css('overflow-x') === 'scroll' || $element.css('overflow-x') === 'auto' || $element.css('overflow-y') === 'scroll' || $element.css('overflow-y') === 'auto')
+					$elementTarget.push($element);
+			}
+			commentManager.overflowTarget = $elementTarget;
+
+			reRenderComment();
+
+		}
+
+
+		function singleWrite(target,msg) {
+			var $commentArea = $('<div class="__comment_area">' + msg + '</div>');
+
+			commonWrite(target,$commentArea);
+		}
+
+		function groupWrite(flag) {
+			for (var opt in flag) {
+				var $commentArea = $('<div class="__comment_area">' + flag[opt] + '</div>');
+
+				commonWrite(opt,$commentArea);
+			}
+		}
+
+		function commonWrite(target,$commentArea) {
+
+			var $commentDom = $('<div class="__ecup_comment"></div>');
+			var $commentBtn = $('<button type="button" class="__comment_btn"><span class="blind">코멘트토글</span></button>');
+
+			$commentDom.append($commentBtn).append($commentArea);
+
+			var $target = $(target);
+
+			if($target.length) {
+				var top = $target.offset().top/$(window).height()*100+'%';
+				var left = $target.offset().left/$(window).width()*100+'%';
+			}
+
+			$commentDom.css({'top': top, 'left': left});
+
+			if($target.css('display')==='none') {
+				$commentDom.css('display','none');
+			}
+
+			else {
+				$commentDom.css('display','block');
+			}
+
+			commentManager.targetDom.append($commentDom);
+
+			$commentBtn.click(function() {
+				var $commentArea = $(this).next('.__comment_area');
+
+				$commentArea.toggle();
+
+				var $commentAreaRightOffset = $commentArea.offset().left+$commentArea.innerWidth();
+
+				if($(window).width() - $commentAreaRightOffset < 20) {
+					$commentArea.css({'right':$commentAreaRightOffset,'left':'auto'});
+				}
+
+			});
+
+			commentManager.targetData.push(target);
+
+		}
+
+		function reRenderComment() {
+			var targetData, commentData, $targetData, $commentData, top, left, $scrolltarget;
+
+			$(window).on("resize scroll",function() {
+				targetData = commentManager.targetData || [];
+				commentData = commentManager.targetDom.find('.__ecup_comment') || [];
+
+				for (var i = 0; i < targetData.length; i++) {
+					$targetData = $(targetData[i]);
+					$commentData = $(commentData[i]);
+
+					if($targetData.css('display')==='none') {
+						$commentData.css('display','none');
+					}
+
+					else {
+						$commentData.css('display','block');
+					}
+
+					if($targetData.length) {
+						top = $targetData.offset().top / $(window).height() * 100 + '%';
+						left = $targetData.offset().left / $(window).width() * 100 + '%';
+					}
+
+					$commentData.css({'top': top, 'left': left});
+
+				}
+			});
+
+			$(document).ready(function() {
+				$scrolltarget = commentManager.overflowTarget;
+
+				for(var t = 0;t < $scrolltarget.length ;t++) {
+					$scrolltarget[t].on("scroll",function() {
+						targetData = commentManager.targetData || [];
+						commentData = commentManager.targetDom.find('.__ecup_comment') || [];
+
+						for(var i = 0; i < targetData.length; i++) {
+							$targetData = $(targetData[i]);
+							$commentData = $(commentData[i]);
+
+							if($targetData.length) {
+								top = $targetData.offset().top / $(window).height() * 100 + '%';
+								left = $targetData.offset().left / $(window).width() * 100 + '%';
+							}
+
+							$commentData.css({'top': top, 'left': left});
+						}
+
+					})
+				}
+			})
+
+		}
+	}
 }, function() {
 	/** jQuery 확장 **/
 
